cac9b26dadf0ad21e3ce2ac18d97e759287a4aaa
thinkdavid <dab4au@virginia.edu>
2019-08-12 16:53:56 -0400

    Added tests and code for sharpen functionality: including accounting for pencil length decrementing by one

--- Pencil.py
+++ Pencil.py
@@ -1,9 +1,17 @@
 class Pencil:
 
     def __init__(self, point_durability, pencil_length, eraser_durability):
-        self.point_durability = point_durability
+        self.initial_point_durability = point_durability
+        self.current_point_durability = point_durability
         self.pencil_length = pencil_length
         self.eraser_durability = eraser_durability
 
     def write(self, text_on_page, text_to_be_written):
         return text_on_page + text_to_be_written
+
+    def sharpen(self):
+        if self.pencil_length > 1:
+            self.current_point_durability = self.initial_point_durability
+            self.pencil_length -= 1
+        else:
+            print("The pencil is too short to sharpen!")
--- PencilDurabilityTests.py
+++ PencilDurabilityTests.py
@@ -2,28 +2,49 @@ import unittest
 from Pencil import Pencil
 
 class TestPencil(unittest.TestCase):
-      def setUp(self):
-          self.pencil = Pencil(10,5,9)
+    def setUp(self):
+        self.pencil = Pencil(10,5,9)
 
 
 class TestInit(TestPencil):
-      def test_initial_point_durability(self):
-          self.assertEqual(self.pencil.point_durability, 10)
+    def test_initial_point_durability(self):
+        self.assertEqual(self.pencil.initial_point_durability, 10)
 
-      def test_initial_eraser_durability(self):
-          self.assertEqual(self.pencil.eraser_durability, 9)
+    def test_initial_point_durability(self):
+        self.assertEqual(self.pencil.current_point_durability, 10)
 
-      def test_initial_pencil_length(self):
-          self.assertEqual(self.pencil.pencil_length, 5)
+    def test_initial_eraser_durability(self):
+        self.assertEqual(self.pencil.eraser_durability, 9)
+
+    def test_initial_pencil_length(self):
+        self.assertEqual(self.pencil.pencil_length, 5)
 
 
 class TestWrite(TestPencil):
-      def test_write_to_empty(self):
-          self.assertEqual(self.pencil.write("", "the lazy dog"), "the lazy dog")
+    def test_write_to_empty(self):
+        self.assertEqual(self.pencil.write("", "the lazy dog"), "the lazy dog")
 
-      def test_write_to_non_empty(self):
-          self.assertEqual(self.pencil.write("the lazy dog ", "jumped over the fox"), "the lazy dog jumped over the fox")
+    def test_write_to_non_empty(self):
+        self.assertEqual(self.pencil.write("the lazy dog ", "jumped over the fox"), "the lazy dog jumped over the fox")
+
+class TestSharpen(TestPencil):
+    def test_sharpen_durability(self):
+        self.pencil.current_point_durability -= 4
+        self.pencil.sharpen()
+        self.assertEqual(self.pencil.current_point_durability, self.pencil.initial_point_durability)
+
+    def test_sharpen_length(self):
+        old_length = self.pencil.pencil_length
+        self.pencil.sharpen()
+        self.assertEqual(old_length-1, self.pencil.pencil_length)
+
+    def test_sharpen_pencil_with_length_0(self):
+        self.pencil.pencil_length = 0
+        self.pencil.current_point_durability -= 4
+        old_durability = self.pencil.current_point_durability
+        self.pencil.sharpen()
+        self.assertEqual(self.pencil.current_point_durability, old_durability)
 
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
