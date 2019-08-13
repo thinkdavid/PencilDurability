bb381cefb8faed1b28443fc9bbb424b1dddf4a1f
thinkdavid <dab4au@virginia.edu>
2019-08-13 12:16:55 -0400

    preliminary tests for edit function

--- PencilDurabilityTests.py
+++ PencilDurabilityTests.py
@@ -1,9 +1,11 @@
 import unittest
+
 from Pencil import Pencil
+
 
 class TestPencil(unittest.TestCase):
     def setUp(self):
-        self.pencil = Pencil(100,5,99)
+        self.pencil = Pencil(100, 5, 99)
         self.pencil2 = Pencil(50, 10, 110)
 
 class TestInit(TestPencil):
@@ -54,20 +56,20 @@ class TestWrite(TestPencil):
 
 class TestPointDegradation(TestPencil):
     def test_point_degradation_capital(self):
-        self.pencil.write("","A")
-        self.assertEqual(self.pencil.current_point_durability, self.pencil.initial_point_durability-2)
+        self.pencil.write("", "A")
+        self.assertEqual(self.pencil.current_point_durability, self.pencil.initial_point_durability - 2)
 
     def test_point_degradation_lowercase(self):
-        self.pencil.write("","a")
-        self.assertEqual(self.pencil.current_point_durability, self.pencil.initial_point_durability-1)
+        self.pencil.write("", "a")
+        self.assertEqual(self.pencil.current_point_durability, self.pencil.initial_point_durability - 1)
 
     def test_point_degradation_number(self):
-        self.pencil.write("","1")
-        self.assertEqual(self.pencil.current_point_durability, self.pencil.initial_point_durability-1)
+        self.pencil.write("", "1")
+        self.assertEqual(self.pencil.current_point_durability, self.pencil.initial_point_durability - 1)
 
     def test_point_degradation_symbol(self):
-        self.pencil.write("","$")
-        self.assertEqual(self.pencil.current_point_durability, self.pencil.initial_point_durability-1)
+        self.pencil.write("", "$")
+        self.assertEqual(self.pencil.current_point_durability, self.pencil.initial_point_durability - 1)
 
     def test_point_degradation_space(self):
         self.pencil.write("", " ")
@@ -78,7 +80,7 @@ class TestPointDegradation(TestPencil):
         self.assertEqual(self.pencil.current_point_durability, self.pencil.initial_point_durability)
 
     def test_point_degradation_string(self):
-        self.pencil.write("","The Lazy Dog") #3 capitals = 6, 7 lowercase = 7
+        self.pencil.write("", "The Lazy Dog")  # 3 capitals = 6, 7 lowercase = 7
         self.assertEqual(self.pencil.current_point_durability, self.pencil.initial_point_durability - 13)
 
 class TestSharpen(TestPencil):
@@ -90,7 +92,7 @@ class TestSharpen(TestPencil):
     def test_sharpen_length(self):
         old_length = self.pencil.pencil_length
         self.pencil.sharpen()
-        self.assertEqual(old_length-1, self.pencil.pencil_length)
+        self.assertEqual(old_length - 1, self.pencil.pencil_length)
 
     def test_sharpen_pencil_with_length_0(self):
         self.pencil.pencil_length = 0
@@ -101,9 +103,12 @@ class TestSharpen(TestPencil):
 
 class TestErase(TestPencil):
     def test_erase_text_on_page(self):
-        self.assertEqual(self.pencil.erase("The Lazy Dog Jumped Over The Fence", "Fence"), "The Lazy Dog Jumped Over The      ")
-        self.assertEqual(self.pencil.erase("The Lazy Dog Jumped Over The Fence", "og"), "The Lazy D   Jumped Over The Fence")
-        self.assertEqual(self.pencil.erase("The Lazy Dog Jumped Over The Fence", "he"), "The Lazy Dog Jumped Over T   Fence")
+        self.assertEqual(self.pencil.erase("The Lazy Dog Jumped Over The Fence", "Fence"),
+                         "The Lazy Dog Jumped Over The      ")
+        self.assertEqual(self.pencil.erase("The Lazy Dog Jumped Over The Fence", "og"),
+                         "The Lazy D   Jumped Over The Fence")
+        self.assertEqual(self.pencil.erase("The Lazy Dog Jumped Over The Fence", "he"),
+                         "The Lazy Dog Jumped Over T   Fence")
 
     def test_erase_text_not_on_page(self):
         self.assertEqual(self.pencil.erase("The Lazy Dog Jumped Over The Fence", "Animals"),
@@ -136,7 +141,7 @@ class TestErase(TestPencil):
     def test_eraser_degradation_without_white_space(self):
         before_eraser = self.pencil.eraser_durability
         self.pencil.erase("The Lazy Dog Jumped Over The Fence", "Fence")
-        self.assertEqual(before_eraser-5, self.pencil.eraser_durability)
+        self.assertEqual(before_eraser - 5, self.pencil.eraser_durability)
 
     def test_eraser_degradation_with_white_space(self):
         before_eraser = self.pencil.eraser_durability
@@ -154,5 +159,19 @@ class TestErase(TestPencil):
         self.assertEqual(self.pencil.erase("The Lazy Dog Jumped Over The Fence", "The"),
                          "The Lazy Dog Jumped Over T   Fence")
 
+class TestEdit(TestPencil):
+    def test_edit_by_replacing_word(self):
+        pass
+    def test_edit_with_collisions(self):
+        pass
+    def test_edit_with_degraded_point(self):
+        pass
+    def test_edit_with_degraded_eraser(self):
+        pass
+    def test_edit_with_point_becoming_degraded(self):
+        pass
+    def test_edit_with_eraser_becoming_degraded(self):
+        pass
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
