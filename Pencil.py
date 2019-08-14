92cff231e7da33e3893d7c05e67b75c55fb71060
thinkdavid <dab4au@virginia.edu>
2019-08-14 15:24:56 -0400

    Changed names and method organization after reading a few chapters of the Clean Code book so as to make functions shorter and more specific

--- Pencil.py
+++ Pencil.py
@@ -6,47 +6,84 @@ class Pencil:
         self.pencil_length = pencil_length
         self.eraser_durability = eraser_durability
 
-    # A Helper Method To Allow Both the Write and Edit Functions To Use
-    def _write_helper(self, text_on_page, text_to_be_written, start_index):
-        new_text = ""
-        page_index = start_index
-        for char in text_to_be_written:
-            if page_index < len(text_on_page):
-                if text_on_page[page_index] != '\n' and text_on_page[page_index] != ' ':
-                    if self.current_point_durability > 0:
-                        new_text += '@'
-                        self.current_point_durability -= 1
-                    else:
-                        new_text += text_on_page[page_index]
-                else:
-                    new_text = self._write_characters(new_text, char)
-                page_index += 1
-            else:
-                new_text = self._write_characters(new_text, char)
-
-        return text_on_page[0: start_index] + new_text
-
-    # A Helper Method To Determine What Kind Of Character/Response is Required When Writing
-    def _write_characters(self, new_text, char):
-        if self.current_point_durability <= 0:
-            new_text += " "
-        elif (char.isupper()):
-            if self.current_point_durability > 1:  # if durability was at 1, we can't write an uppercase
-                self.current_point_durability -= 2
-                new_text += char
-            else:
-                new_text += " "
-        elif (char == ' ' or char == '\n'):
-            self.current_point_durability -= 0
-            new_text += char
-        else:
-            self.current_point_durability -= 1
-            new_text += char
-        return new_text
-
     # User Callable Method -- All of the Logic is in the WriteHelper and WriteCharacters Private Methods
     def write(self, text_on_page, text_to_be_written):
         return self._write_helper(text_on_page, text_to_be_written, len(text_on_page))
+
+    # A Helper Method To Allow Both the Write and Edit Functions To Use
+    def _write_helper(self, text_on_page, text_to_be_written, start_index):
+        written_text = ""
+        current_index = start_index
+        for char_to_write in text_to_be_written:
+            if current_index < len(text_on_page):  # if an edit in between a string
+                current_character = text_on_page[current_index]
+                written_text += self._write_characters_edit(current_character, char_to_write)
+                current_index += 1
+            else:  # if appending to the page, just write the characters
+                written_text += self._write_characters(char_to_write)
+
+        return text_on_page[0: start_index] + written_text
+
+    def _write_characters_edit(self, current_character, char_to_write):
+        if current_character != '\n' and current_character != ' ':  # collision
+            if self.current_point_durability > 0:  # if the pencil has a point
+                self.current_point_durability -= 1
+                return '@'
+            else:  # no point, we can't write anything so the page stays as is
+                return current_character
+        else:  # if no collision, write the character in the space and degrade the point
+            return self._write_characters(char_to_write)
+
+    # A Helper Method To Determine What Kind Of Character/Response is Required When Writing
+    def _write_characters(self, char_to_write):
+        if self.current_point_durability <= 0:
+            return " "
+        elif char_to_write.isupper():
+            if self.current_point_durability > 1:  # if durability was at 1, we can't write an uppercase
+                self.current_point_durability -= 2
+                return char_to_write
+            else:
+                return " "
+        elif char_to_write == ' ' or char_to_write == '\n':
+            return char_to_write
+        else:
+            self.current_point_durability -= 1
+            return char_to_write
+
+    # User Callable Method to Erase -- All of the Logic is in the EraseHelper Method
+    def erase(self, text_on_page, text_to_erase):
+        word_erased_index, new_text = self._erase_helper(text_on_page, text_to_erase)
+        if word_erased_index == -1:
+            return text_on_page
+        else:
+            return new_text.rstrip()
+
+    # A Helper Method To Allow Both the Erase and Edit Functions to Use It
+    def _erase_helper(self, text_on_page, text_to_erase):
+        text_to_erase.strip(" ")
+        text_to_erase.strip("\n")
+        word_to_erase_index = text_on_page.lower().rfind(text_to_erase.lower())  # index where the word was found.
+        if word_to_erase_index == -1 or len(text_to_erase) < 1:  # if the word wasn't found, we do nothing
+            return -1, ""
+        erased_space = self._erase_characters(word_to_erase_index, text_on_page, text_to_erase)
+        text_before_erase = text_on_page[0: word_to_erase_index + len(text_to_erase) - len(erased_space)]
+        text_after_erase = text_on_page[word_to_erase_index + len(text_to_erase):]
+        return word_to_erase_index, text_before_erase + erased_space + text_after_erase
+
+    # A Helper Method to Calculate the Number of Erased Spaces
+    def _erase_characters(self, word_to_erase_index, text_on_page, text_to_erase):
+        erased_space = ""
+        erase_start_index = word_to_erase_index + len(text_to_erase) - 1
+        for i in range(erase_start_index, word_to_erase_index - 1, -1):
+            if text_on_page[i] == ' ' or text_on_page[i] == '\n':
+                erased_space += text_on_page[i]
+                continue
+            if self.eraser_durability > 0:
+                erased_space += " "
+                self.eraser_durability -= 1
+            else:  # we erase until the eraser breaks
+                break
+        return erased_space
 
     # User Callable Method to sharpen the pencil
     def sharpen(self):
@@ -56,37 +93,7 @@ class Pencil:
         else:
             print("Can't sharpen this pencil!")
 
-    # A Helper Method To Allow Both the Erase and Edit Functions to Use It
-    def _erase_helper(self, text_on_page, text_to_erase):
-        text_to_erase.strip(" ")
-        text_to_erase.strip("\n")
-        index = text_on_page.lower().rfind(text_to_erase.lower())  # index where the word was found.
-        if index == -1 or len(text_to_erase) < 1:
-            return ((-1, ""))
-        new_text = ""  # the number of erased characters
-        for i in range(index + len(text_to_erase) - 1, index - 1, -1):  # the string length is the max amount of erasure
-            if text_on_page[i] == ' ' or text_on_page[i] == '\n':
-                new_text += text_on_page[i]
-                continue
-            if self.eraser_durability > 0:
-                new_text += " "
-                self.eraser_durability -= 1
-            else:
-                break
-
-        return ((index, text_on_page[0: index + len(text_to_erase) - len(new_text)] \
-                   + new_text \
-                   + text_on_page[index + len(text_to_erase):]))
-
-    # User Callable Method to Erase -- All of the Logic is in the EraseHelper Method
-    def erase(self, text_on_page, text_to_erase):
-        index, new_text = self._erase_helper(text_on_page, text_to_erase)
-        if index == -1:
-            return text_on_page
-        else:
-            return new_text.rstrip()
-
-    #User Callable Method to edit code -- Logic is based on both _write_helper and _erase_helper
+    # User Callable Method to edit code -- Logic is based on both _write_helper and _erase_helper
     def edit(self, text_on_page, text_to_edit, text_to_replace_with):
         index, after_erase_text = self._erase_helper(text_on_page, text_to_edit)
         if index == -1:
--- PencilDurabilityTests.py
+++ PencilDurabilityTests.py
@@ -6,25 +6,25 @@ from Pencil import Pencil
 class TestPencil(unittest.TestCase):
     def setUp(self):
         self.pencil = Pencil(100, 5, 99)
-        self.pencil2 = Pencil(50, 10, 110)
+        self.pencil_2 = Pencil(20, 3, 30)
 
 
 class TestInit(TestPencil):
     def test_initial_point_durability(self):
         self.assertEqual(self.pencil.initial_point_durability, 100)
-        self.assertEqual(self.pencil2.initial_point_durability, 50)
+        self.assertEqual(self.pencil_2.initial_point_durability, 20)
 
     def test_current_point_durability_before_writing(self):
         self.assertEqual(self.pencil.current_point_durability, 100)
-        self.assertEqual(self.pencil2.current_point_durability, 50)
+        self.assertEqual(self.pencil_2.current_point_durability, 20)
 
     def test_initial_eraser_durability(self):
         self.assertEqual(self.pencil.eraser_durability, 99)
-        self.assertEqual(self.pencil2.eraser_durability, 110)
+        self.assertEqual(self.pencil_2.eraser_durability, 30)
 
     def test_initial_pencil_length(self):
         self.assertEqual(self.pencil.pencil_length, 5)
-        self.assertEqual(self.pencil2.pencil_length, 10)
+        self.assertEqual(self.pencil_2.pencil_length, 3)
 
 
 class TestWrite(TestPencil):
@@ -133,8 +133,9 @@ class TestErase(TestPencil):
                          "The Lazy Dog Jumped Over T   Fence")
         self.assertEqual(self.pencil.erase("The Lazy Dog Jumped Over A Fence", "The"),
                          "    Lazy Dog Jumped Over A Fence")
-        self.assertEqual(self.pencil.erase("How much wood would a woodchuck chuck if a woodchuck could       wood", "chuck"),
-                         "How much wood would a woodchuck chuck if a wood      could       wood")
+        self.assertEqual(
+            self.pencil.erase("How much wood would a woodchuck chuck if a woodchuck could       wood", "chuck"),
+            "How much wood would a woodchuck chuck if a wood      could       wood")
 
     def test_erase_text_not_on_page(self):
         self.assertEqual(self.pencil.erase("The Lazy Dog Jumped Over The Fence", "Animals"),
@@ -243,7 +244,7 @@ class TestEdit(TestPencil):
     def test_edit_with_start_in_page_continue_to_new(self):
         # basically test when we start the edit inside the text on the page, then go into new
         edited_string = self.pencil.edit("I Love Pancakes", "Pancakes",
-                                        "Waffles, mostly, but Pancakes are a solid choice")
+                                         "Waffles, mostly, but Pancakes are a solid choice")
         self.assertEqual(edited_string, "I Love Waffles, mostly, but Pancakes are a solid choice")
 
 
