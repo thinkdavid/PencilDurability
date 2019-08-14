96306e4e5a16ff43d30eb0cf8ead8b456ff28156
thinkdavid <dab4au@virginia.edu>
2019-08-14 16:02:11 -0400

    Working on cleaning up methods based on readings from the book

--- Pencil.py
+++ Pencil.py
@@ -6,25 +6,22 @@ class Pencil:
         self.pencil_length = pencil_length
         self.eraser_durability = eraser_durability
 
-    # User Callable Method -- All of the Logic is in the WriteHelper and WriteCharacters Private Methods
     def write(self, text_on_page, text_to_be_written):
         return self._write_helper(text_on_page, text_to_be_written, len(text_on_page))
 
-    # A Helper Method To Allow Both the Write and Edit Functions To Use
     def _write_helper(self, text_on_page, text_to_be_written, start_index):
         written_text = ""
         current_index = start_index
         for char_to_write in text_to_be_written:
-            if current_index < len(text_on_page):  # if an edit in between a string
+            if current_index < len(text_on_page):
                 current_character = text_on_page[current_index]
                 written_text += self._check_for_collision(current_character, char_to_write)
                 current_index += 1
-            else:  # if appending to the page, just write the characters
+            else:
                 written_text += self._write_characters(char_to_write)
 
         return text_on_page[0: start_index] + written_text
 
-    # Checks for collisions
     def _check_for_collision(self, current_character, char_to_write):
         if current_character != '\n' and current_character != ' ':  # collision
             if self.current_point_durability > 0:  # if the pencil has a point
@@ -33,20 +30,21 @@ class Pencil:
                 return current_character
         return self._write_characters(char_to_write)
 
-    # Performs the point degradation and returns character to be written
     def _write_characters(self, char_to_write):
         if self.current_point_durability <= 0:
             char_to_write = " "
         elif char_to_write.isupper():
             if self.current_point_durability > 1:  # if durability was at 1, we can't write an uppercase
-                self.current_point_durability -= 2
+                self._degrade_point(2)
             else:
                 char_to_write = " "
         elif char_to_write != ' ' and char_to_write != '\n':
-            self.current_point_durability -= 1
+            self._degrade_point(1)
         return char_to_write
 
-    # User Callable Method to Erase -- All of the Logic is in the EraseHelper Method
+    def _degrade_point(self, num_to_degrade):
+        self.current_point_durability -= num_to_degrade
+
     def erase(self, text_on_page, text_to_erase):
         word_erased_index, new_text = self._erase_helper(text_on_page, text_to_erase)
         if word_erased_index == -1:
@@ -54,7 +52,6 @@ class Pencil:
         else:
             return new_text.rstrip()
 
-    # A Helper Method To Allow Both the Erase and Edit Functions to Use It
     def _erase_helper(self, text_on_page, text_to_erase):
         text_to_erase.strip(" ")
         text_to_erase.strip("\n")
@@ -66,7 +63,6 @@ class Pencil:
         text_after_erase = text_on_page[word_to_erase_index + len(text_to_erase):]
         return word_to_erase_index, text_before_erase + erased_space + text_after_erase
 
-    # A Helper Method to Calculate the Number of Erased Spaces
     def _erase_characters(self, word_to_erase_index, text_on_page, num_to_erase):
         erased_space = ""
         erase_start_index = word_to_erase_index + num_to_erase - 1
@@ -76,12 +72,14 @@ class Pencil:
                 continue
             if self.eraser_durability > 0:
                 erased_space += " "
-                self.eraser_durability -= 1
-            else:  # we erase until the eraser breaks
+                self._degrade_eraser()
+            else:  # if eraser degrades, we break early
                 break
         return erased_space
 
-    # User Callable Method to sharpen the pencil
+    def _degrade_eraser(self):
+        self.eraser_durability -= 1
+
     def sharpen(self):
         if self.pencil_length >= 1 and self.current_point_durability != self.initial_point_durability:
             self.current_point_durability = self.initial_point_durability
@@ -89,10 +87,9 @@ class Pencil:
         else:
             print("Can't sharpen this pencil!")
 
-    # User Callable Method to edit code -- Logic is based on both _write_helper and _erase_helper
-    def edit(self, text_on_page, text_to_edit, text_to_replace_with):
-        index, after_erase_text = self._erase_helper(text_on_page, text_to_edit)
-        if index == -1:
+    def edit(self, text_on_page, text_to_replace, text_to_insert):
+        index_of_replaced_word, after_erase_text = self._erase_helper(text_on_page, text_to_replace)
+        if index_of_replaced_word == -1:
             return text_on_page
-        edited_text = self._write_helper(after_erase_text, text_to_replace_with, index)
-        return edited_text + after_erase_text[index + len(text_to_replace_with):].rstrip()
+        edited_text = self._write_helper(after_erase_text, text_to_insert, index_of_replaced_word)
+        return edited_text + after_erase_text[index_of_replaced_word + len(text_to_insert):].rstrip()
